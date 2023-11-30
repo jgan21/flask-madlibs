3905a21c15620f69acadbe48bfa9f434511c496c
Jia Rong Gan <jiaronggan21@gmail.com>
2023-11-29 18:23:40 -0600

    added view function to app.py

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def question_page():
+    """Home page to Madlibs game with questions listed"""
+
+    # check how many inputs are in the story list
+    # loop through the list
+    # render the amount of inputs needed
+    prompt_block = ""
+    for prompt in silly_story.prompts:
+        prompt_block += render_template("madlib_prompt.html", prompt = prompt)
+    return prompt_block
+
